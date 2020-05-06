7aa66c3d054b856890356c92aff921bccbb79236
uhooi <uhooi@server.fake>
2020-05-06 18:44:05 +0900

    Firestoreのセキュリティルールとユーザー更新のテストを追加

--- rules.test.ts
+++ rules.test.ts
@@ -9,7 +9,7 @@ const databaseName = 'moga-firestore'
 const rules = fs.readFileSync('./firestore.rules', 'utf8')
 
 // firestore client for admin
-const adminDB = firebase.initializeAdminApp({ projectId: projectID, databaseName })
+const adminDB = firebase.initializeAdminApp({ projectId: projectID, databaseName }).firestore()
 
 type Auth = {
   uid?: string,
@@ -20,19 +20,83 @@ type Auth = {
 // firestore client for client-side user
 const clientDB = (auth?: Auth) => firebase.initializeTestApp({ projectId: projectID, databaseName, auth }).firestore()
 
+const serverTimestamp = () => firebase.firestore.FieldValue.serverTimestamp()
+
 // setup and clean up
 
 beforeAll(async () => {
-  await firebase.loadFirestoreRules({ projectId: projectID, rules });
+  await firebase.loadFirestoreRules({ projectId: projectID, rules })
 })
 
 beforeEach(async () => {
-  await firebase.clearFirestoreData({ projectId: projectID });
+  await firebase.clearFirestoreData({ projectId: projectID })
 })
 
 afterAll(async () => {
-  await Promise.all(firebase.apps().map(app => app.delete()));
+  await Promise.all(firebase.apps().map(app => app.delete()))
 })
 
-describe('write test case', () => {
+describe('/users', () => {
+  describe('update', () => {
+    const userId = randomID()
+    const db = clientDB({ uid: userId})
+
+    describe('「groupCount」が更新されている', () => {
+      describe('「updateAt」がリクエストした時間と一致していない', () => {
+        it('更新できない', async () => {
+          await configureTestData('users', userId)
+
+          await firebase.assertFails(db.collection('users').doc(userId).set({
+            groupCount: 1,
+            updatedAt: 'failed'
+          }))
+        })
+      })
+      describe('「updateAt」がリクエストした時間と一致している', () => {
+        it('更新できない', async () => {
+          await configureTestData('users', userId)
+
+          await firebase.assertFails(db.collection('users').doc(userId).set({
+            groupCount: 1,
+            updatedAt: serverTimestamp()
+          }))
+        })
+      })
+    })
+    describe('「groupCount」が更新されていない', () => {
+      describe('「updateAt」がリクエストした時間と一致していない', () => {
+        it('更新できない', async () => {
+          await configureTestData('users', userId)
+
+          await firebase.assertFails(db.collection('users').doc(userId).set({
+            groupCount: 0,
+            updatedAt: 'failed'
+          }))
+        })
+      })
+      describe('「updateAt」がリクエストした時間と一致している', () => {
+        it('更新できる', async () => {
+          await configureTestData('users', userId)
+
+          await firebase.assertSucceeds(db.collection('users').doc(userId).set({
+            groupCount: 0,
+            updatedAt: serverTimestamp()
+          }))
+        })
+      })
+    })
+  })
 })
+
+function configureTestData(collectionId: string, documentId: string) {
+  const db = adminDB
+
+  return db.collection(collectionId).doc(documentId).set(createTestUser())
+}
+
+function createTestUser() {
+  return {
+    groupCount: 0,
+    updatedAt: serverTimestamp()
+  }
+}
